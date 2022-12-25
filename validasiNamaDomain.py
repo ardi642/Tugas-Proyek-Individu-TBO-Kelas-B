39ac05e0992445f26c4f48d24be569aa0a681e78
ardi642 <121119730+ardi642@users.noreply.github.com>
2022-12-25 23:46:50 +0700

    Update validasiNamaDomain.py

--- validasiNamaDomain.py
+++ validasiNamaDomain.py
@@ -35,7 +35,7 @@ while True:
     print()
     ulang = input("Apakah ingin memvalidasi nama domain lagi ? (y/t) : ")
 
-    if (ulang == "y" or ulang == 'Y"):
+    if (ulang == "y"):
         # os.system("cls")
         print()
         continue
